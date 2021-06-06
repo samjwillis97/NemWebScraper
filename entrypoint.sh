00541b5cb4041c33fc8598118d1e060e052c8cc5
samjwillis97 <samjwillis97@gmail.com>
2021-06-06 23:27:31 +1000

    forgot to remove CMD line6 entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ echo "Docker Container has Started"
 
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
-CMD busybox syslogd -C
+busybox syslogd -C
 cron -L 2 -f
